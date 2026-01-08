7ff720d51dcd2f94e4b8594a85b58f9fadde8e09
Cherve <ch3rv3@gmail.com>
2026-01-08 00:38:35 -0500

    Initial setup of a cli agent. hardcoded prompt to test response success

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    print("Hello from cli-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == "":
+        raise RuntimeError("gemini api key not found")
+    client = genai.Client(api_key=api_key)
+
+    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=contents)
+
+    if response.usage_metadata != None:
+
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    else:
+        raise RuntimeError("usage metadata returned None")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
