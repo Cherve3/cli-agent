45ea63ed2de31bc90d33d1b9f9ac54493bfdd5bf
Cherve <ch3rv3@gmail.com>
2026-01-08 01:02:17 -0500

    user input prompt as command line argument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import argparse
+
 from dotenv import load_dotenv
 from google import genai
 
@@ -13,16 +15,20 @@ def main():
 
     contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=contents)
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
 
     if response.usage_metadata != None:
-
         prompt_tokens = response.usage_metadata.prompt_token_count
         response_tokens = response.usage_metadata.candidates_token_count
         print(f"Prompt tokens: {prompt_tokens}")
         print(f"Response tokens: {response_tokens}")
     else:
         raise RuntimeError("usage metadata returned None")
+
     print(response.text)
 
 if __name__ == "__main__":
